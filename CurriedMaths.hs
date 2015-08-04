70e9793e63f23ad8b9576cb18941cf5ebb67e314
Liam Hackett <liamjhackett@gmail.com>
2015-08-04 16:20:17 +0100

    Update TupleMaths and CurriedMaths scripts

--- CurriedMaths.hs
+++ CurriedMaths.hs
@@ -18,3 +18,27 @@ roots a b c = ((-b + t) /a', (-b - t)/a')
   where
     t = b ^ 2 - 4 * a * c
     a' = 2 * a
+
+min3 :: Int -> (Int -> (Int -> Int))
+min3 x y z = min (min x y) z
+
+max4 :: Int -> (Int -> (Int -> (Int -> Int)))
+max4 w x y z  = max (max w x) (max y z)
+
+allTheSame :: Int -> (Int -> (Int -> Bool))
+allTheSame x y z = x==y&&y==z&&x==z
+
+anyTwoTheSame :: Int -> (Int -> (Int -> Bool))
+anyTwoTheSame x y z = x==y||x==z||y==z
+
+noneTheSame :: Int -> (Int -> (Int -> Bool))
+noneTheSame x y z = x/=y&&y/=z&&x/=z
+
+finalSpeed :: Double -> (Double -> (Double -> Double))
+finalSpeed s a t = s + a * t
+
+intoList :: Int -> (Int -> (Int -> [Int]))
+intoList x y z = [x,y,z]
+
+intoTuple :: any -> (any -> (any -> (any,any,any))) 
+intoTuple x y z = (x,y,z)
